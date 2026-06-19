5f12a9876c61289384473fb69007aab7799f4c90
mahoora <maherpadr@gmail.com>
2026-06-19 04:10:47 +0300

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -624,7 +624,7 @@ async function startBridge() {
 
   sock.ev.on('messages.upsert', async ({ messages }) => {
     try { 
-      await new Promise(r => setTimeout(r, 2000)); 
+      await new Promise(r => setTimeout(r, 5000)); 
       for (const msg of messages) {
       if (!msg.key || msg.key.fromMe) continue;
       const jid = msg.key.remoteJid;
